d6c046e85f5d7f6c0b9495284596a6cbcfe8de73
Pulsatio <gustavo.orosco@utec.edu.pe>
2021-08-05 17:35:05 -0500

    kruskal

--- Graph/Algorithms/kruskal.h
+++ Graph/Algorithms/kruskal.h
@@ -2,7 +2,6 @@
 #define PROYECTO_DE_CURSO_RUNTIME_TERROR_KRUSKAL_H
 
 
-
 template <typename VT,typename ET>
 Graph<VT,ET> kruskal(Graph<VT,ET>& graph){
     if(graph.is_directed) return Graph<VT,ET>();
@@ -19,28 +18,31 @@ Graph<VT,ET> kruskal(Graph<VT,ET>& graph){
         parent[v] = u;
         return true;
     };
+    // END DSU
     Graph<VT,ET> ans;
     for(int i = 0; i < n; i++){
         ans.insertVertex(graph.names[i],graph.data[i]);
     }
     vector<pair<ET,int>> edges;
     vector<pair<int,int>> list_edges(graph.cur_edge);
-    for(int i = 0; i < graph.cur_edge; i++) edges.push_back({graph.weight[i],i});
 
     for(auto it:graph.num_edges){
         list_edges[it.second] = it.first;
+        edges.push_back({graph.weight[it.second],it.second});
     }
 
     sort(edges.begin(),edges.end());
     int cont = 0;
+    long long answer = 0;
     for(int i = 0; i < graph.cur_edge; i++){
         int u = list_edges[ edges[i].second ].first, v = list_edges[ edges[i].second ].second;
         if(uni(u,v)){
+   
+            answer += graph.weight[ edges[i].second];
             ans.createEdge(graph.names[u],graph.names[v],graph.weight[ edges[i].first ]);
             cont++;
         }
     }
-    //if(cont < n-1) return Graph<VT,ET>();
     return ans;
 
 }
