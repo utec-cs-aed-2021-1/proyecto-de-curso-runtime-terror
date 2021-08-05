3ad1c0701a735808ca02921e39974421d57567fd
Pulsatio <gustavo.orosco@utec.edu.pe>
2021-08-05 18:03:35 -0500

    fix

--- Graph/Algorithms/bfs.h
+++ Graph/Algorithms/bfs.h
@@ -3,14 +3,18 @@
 #include <vector>
 
 template <typename VT,typename ET>
-vector<int> bfs_graph(Graph<VT,ET>& graph, int root = 0){
+vector<string> bfs_graph(Graph<VT,ET>& graph,string root){
     vector<bool> vis(graph.cur_vertex,false);
-    vector<int> order;
-    vis[root] = true;
+    vector<string> order;
+    if(graph.vertexes.find(root) == graph.vertexes.end()) return order;
+    int r = graph.vertexes[root];
+    vis[r] = true;
     queue<int> q;
+    q.push(r);
     while(!q.empty()){
         int u = q.front();
-        order.push_back(u);
+        q.pop();
+        order.push_back(graph.names[u]);
         for(auto v:graph.adj[u]){
             if(!vis[v.first]){
                 vis[v.first] = true;
@@ -20,5 +24,6 @@ vector<int> bfs_graph(Graph<VT,ET>& graph, int root = 0){
     }
     return order;
 }
+
 
 #endif
