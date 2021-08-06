feb3985d71ed1be260016ab5c7c9f1c1dce677e7
Dom0530 <55152988+Dom0530@users.noreply.github.com>
2021-08-05 19:50:56 -0500

    Update astar.h

--- Graph/Algorithms/astar.h
+++ Graph/Algorithms/astar.h
@@ -1,8 +1,8 @@
 #ifndef PROYECTO_DE_CURSO_RUNTIME_TERROR_ASTAR_H
 #define PROYECTO_DE_CURSO_RUNTIME_TERROR_ASTAR_H
-#include "./Graph/graph.h"
 
-friend void Astar(Graph<TV,TE> g) {
+template <typename VT,typename ET>
+void Astar(Graph<VT,ET> g) {
 
 }
 
